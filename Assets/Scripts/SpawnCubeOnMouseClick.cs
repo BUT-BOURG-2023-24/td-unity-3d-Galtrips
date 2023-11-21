d9295e2649f652796a386f7d69b52727f56421c3
Yohan LAPIERRE <yohan.lapierre@mb-expansion.fr>
2023-11-21 17:40:03 +0100

    Push

--- Assets/Scripts/SpawnCubeOnMouseClick.cs
+++ Assets/Scripts/SpawnCubeOnMouseClick.cs
@@ -2,30 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class SpawnCubeOnMouseClick : MonoBehaviour
 {
     public InputActionReference clickInputRef = null;
     public LayerMask layerMask;
     public GameObject cubePrefab = null;
+    public Joystick joystik = null;
 
     private float spawnHeightOffset = 0.5f;
 
     private void OnEnable()
     {
         clickInputRef.action.performed += onClick;
+
+        EnhancedTouch.TouchSimulation.Enable();
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+
+        EnhancedTouch.Touch.onFingerDown += OnFingerDown;
     }
 
     private void OnDisable()
     {
         clickInputRef.action.performed -= onClick;
+        //EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.TouchSimulation.Disable();
+
+        EnhancedTouch.Touch.onFingerDown -= OnFingerDown;
     }
 
     private void onClick(InputAction.CallbackContext obj)
     {
-        Vector3 mousePosition = Input.mousePosition;
+        //spawnCube(Input.mousePosition);
+    }
+
+    private void OnFingerDown(EnhancedTouch.Finger finger)
+    {
+        RectTransform joystikCoord = (joystik.transform as RectTransform);
+
+        if (finger.screenPosition.x > joystikCoord.offsetMin.x && finger.screenPosition.x < joystikCoord.offsetMax.x
+                       && finger.screenPosition.y > joystikCoord.offsetMin.y && finger.screenPosition.y < joystikCoord.offsetMax.y)
+        {
+            return;
+        }
+
+        spawnCube(finger.screenPosition);
+    }
+
+    private void spawnCube(Vector2 screenPosition)
+    {
+        Vector3 mousePosition = screenPosition;
         Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-        
+
         //Debug.DrawRay(ray.origin, ray.direction, Color.blue, 100.0f);
         RaycastHit hitInfo;
         if (Physics.Raycast(ray, out hitInfo, 1000f, layerMask))
@@ -35,6 +64,5 @@ public class SpawnCubeOnMouseClick : MonoBehaviour
             GameObject cube = GameObject.Instantiate(cubePrefab, spawnpoint, Quaternion.identity);
 
         }
-
     }
 }
